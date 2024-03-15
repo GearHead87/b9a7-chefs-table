870356ec4ed4954c0c619a800ecf6f8534cc3284
GearHead87 <hosanulislam87@gmail.com>
2024-03-15 22:33:04 +0600

    navbar and banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import Banner from "./Banner/Banner";
+import Navbar from "./Navbar/Navbar";
+
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <div className="max-w-screen-xl mx-auto">
+      <Navbar></Navbar>
+      <Banner></Banner>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bannerBG': "url('./src/assets/BannerBG.svg')",
+      }
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"],
+  },
 }
 
