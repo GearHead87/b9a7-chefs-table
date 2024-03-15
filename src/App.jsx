2fd8937fb4736e32a20810110984a16352f15272
GearHead87 <hosanulislam87@gmail.com>
2024-03-15 23:22:37 +0600

    banner section data fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Banner from "./Banner/Banner";
 import Navbar from "./Navbar/Navbar";
+import RecipeSection from "./RecipeSection/RecipeSection";
 
 function App() {
-
 
   return (
     <div className="max-w-screen-xl mx-auto">
       <Navbar></Navbar>
       <Banner></Banner>
+      <RecipeSection></RecipeSection>
     </div>
   )
 }
 
-export default App
+export default App;
