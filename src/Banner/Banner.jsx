78b0d4bc9781a3a11f7734cd56d6f0f715c907c5
GearHead87 <hosanulislam87@gmail.com>
2024-03-16 22:54:56 +0600

    banner margin added

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,6 +1,6 @@
 const Banner = () => {
     return (
-        <div className="bg-[url('/BannerBG.svg')] bg-no-repeat bg-center text-center px-4 lg:px-64 py-20 lg:py-32 mt-4 lg:mt-10 rounded-2xl flex flex-col items-center justify-center">
+        <div className="bg-[url('/BannerBG.svg')] bg-no-repeat bg-center text-center px-4 lg:px-64 py-20 lg:py-32 mt-4 lg:mt-10 rounded-2xl mx-4 flex flex-col items-center justify-center">
             <h2 className="text-xl lg:text-6xl font-bold text-white">Discover an exceptional cooking class tailored for you!</h2>
             <p className="text-white text-base lg:text-lg mt-4">Discover the joy of cooking with fresh ingredients and elevate your culinary skills to new heights in a welcoming and supportive environment.</p>
             <div className="flex flex-col lg:flex-row gap-4 mt-4">
